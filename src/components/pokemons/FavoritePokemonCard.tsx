4dc705d3814d6bdaea962f550a50f2ab525054c4
Fernando Herrera <fernando.herrera85@gmail.com>
2024-06-13 11:51:47 -0400

    refactor: Update FavoritePokemons component to use FavoritePokemonCard

---
+++ src/components/pokemons/FavoritePokemonCard.tsx
@@ -0,0 +1,39 @@
+import type { FavoritePokemon } from '@interfaces/favorite-pokemon';
+import { Show, createSignal, type Component } from 'solid-js';
+
+interface Props {
+  pokemon: FavoritePokemon;
+}
+
+export const FavoritePokemonCard: Component<Props> = ({ pokemon }) => {
+  const [isVisible, setIsVisible] = createSignal(true);
+
+  const imageSrc = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemon.id}.png`;
+
+  const deleteFavorite = () => {
+    const favorites = JSON.parse(
+      localStorage.getItem('favorites') ?? '[]'
+    ) as FavoritePokemon[];
+
+    const newFavorites = favorites.filter((p) => p.id !== pokemon.id);
+
+    localStorage.setItem('favorites', JSON.stringify(newFavorites));
+    setIsVisible(false);
+  };
+
+  return (
+    <Show when={isVisible()}>
+      <div class="flex flex-col justify-center items-center">
+        <a href={`/pokemons/${pokemon.name}`}>
+          <img src={imageSrc} alt={pokemon.name} width="96" height="96" />
+          <p class="capitalize">
+            #{pokemon.id} {pokemon.name}
+          </p>
+        </a>
+        <button onClick={deleteFavorite} class="text-red-400">
+          Borrar
+        </button>
+      </div>
+    </Show>
+  );
+};
--- src/components/pokemons/FavoritePokemons.tsx
+++ src/components/pokemons/FavoritePokemons.tsx
@@ -1,5 +1,7 @@
+import NavBar from '@components/shared/NavBar.astro';
 import type { FavoritePokemon } from '@interfaces/favorite-pokemon';
 import { For, createSignal } from 'solid-js';
+import { FavoritePokemonCard } from './FavoritePokemonCard';
 
 const getLocalStoragePokemons = (): FavoritePokemon[] => {
   const favoritePokemons = JSON.parse(
@@ -14,7 +16,9 @@ export const FavoritePokemons = () => {
 
   return (
     <div class="grid grid-cols-2 sm:grid-cols-4">
-      <For each={pokemons()}>{(pokemon) => <h1>{pokemon.name}</h1>}</For>
+      <For each={pokemons()}>
+        {(pokemon) => <FavoritePokemonCard pokemon={pokemon} />}
+      </For>
     </div>
   );
 };
